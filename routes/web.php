1ed29f69a6b77937d2377ccd258d725dfce02892
loredanapatricia <loredana.prb@gmail.com>
2021-02-22 14:01:18 +0100

    modified routes coments

--- routes/web.php
+++ routes/web.php
@@ -27,15 +27,15 @@ Route::group([
 });
 
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
