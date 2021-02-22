a3faa63709ba6d72c1d6b520810d14e7e7ded818
J-ordiEs <70259721+J-ordiEs@users.noreply.github.com>
2021-02-22 13:45:12 +0100

    ok

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,21 +6,21 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
-w | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix
-    .js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-        require('postcss-nested'),
-        require('autoprefixer'),
-    ]);
+  .js('resources/js/app.js', 'public/js')
+  .postCss('resources/css/app.css', 'public/css', [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('postcss-nested'),
+    require('autoprefixer'),
+  ]);
 
 if (mix.inProduction()) {
-    mix
-        .version();
+  mix
+    .version();
 }
